f133d85f8b0b71d3be592776327106b0b95d4856
Cara Tiller <ctiller24@biomedscienceacademy.org>
2020-12-10 16:50:12 +0000

    I added the todays part of the inputs

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ Author: Cara Tiller
 birthyear=int(input("When was your birthyear? exp. '2000' :  "))
 birthmonth=int(input("When is your birthmonth number? exp. July = '7': "))
 birthday= int(input("When is your birthday?: "))
+print("Input todays date like you did for your birthday ")
+todayyear=int(input("What is todays year?: "))
+todaysmonth=int(input("What is todays month?: "))
+todaysday=int(input("What is todays day?: "))
+
+
+
+
