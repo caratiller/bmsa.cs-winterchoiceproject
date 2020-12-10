ecb8a09c04a71a4e4386ba79e38e1cb41cea7777
Cara Tiller <ctiller24@biomedscienceacademy.org>
2020-12-10 16:43:40 +0000

    so far i have done the inputs

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+WinterChoiceProject 
+Author: Cara Tiller 
+
+"""
+
+#this asks the user to input their birthday by year month and day
+birthyear=int(input("When was your birthyear? exp. '2000' :  "))
+birthmonth=int(input("When is your birthmonth number? exp. July = '7': "))
+birthday= int(input("When is your birthday?: "))
